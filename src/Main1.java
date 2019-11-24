ea4287e1ceeda1624642e5058eaa144d6f1aa241
Виктория Беспалова <f>
2019-11-24 18:10:22 +0300

    task1

---
+++ src/Main1.java
@@ -0,0 +1,13 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person1 p = new Person1("Test", 3);
+		System.out.println(p);
+		p.setAge(5);
+		p.setName("Vasya");
+		System.out.println(p.toString());
+	}
+
+}
